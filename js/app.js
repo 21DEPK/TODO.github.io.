bcfaf7aed968757d89fe94389238c9df06f9d86a
21DEPK <110716963+21DEPK@users.noreply.github.com>
2023-10-26 06:01:00 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,16 @@
 let input = document.querySelector("input");
 let add = document.querySelector(".inputtasks button");
 let inputTasks = document.querySelector(".tasks");
-let del = inputTasks.children;
 
 let Add = () => {
     if (input.value !== '') {
         inputTasks.innerHTML += `<div class="individualTask">
         <p>${input.value}</p>
-        <button class="delete"><span class="material-symbols-outlined">delete</span></button>
+        <button class="delete" onclick="this.parentElement.remove();"><span class="material-symbols-outlined">delete</span></button>
         </div>`;
         input.value = '';
-        deleteDynamicTasks();
     } else {
         alert(`Task is Empty!`);
-    }
-}
-function deleteDynamicTasks() {
-    if (del.length !== 0) {
-        for (let i = 0; i < del.length; i++) {
-            del[i].lastElementChild.addEventListener("click", () => {
-                del[i].remove();
-            })
-        }
     }
 }
 add.addEventListener("click", Add);
